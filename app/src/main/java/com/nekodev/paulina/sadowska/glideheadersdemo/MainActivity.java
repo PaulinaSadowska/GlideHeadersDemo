c3e6ac6f065081920229a14942b5f76b2aafc26b
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-09-03 15:32:31 +0200

    ImageView name changed

--- app/src/main/java/com/nekodev/paulina/sadowska/glideheadersdemo/MainActivity.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/glideheadersdemo/MainActivity.java
@@ -8,21 +8,21 @@ import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView catImage;
+    private ImageView catImageView;
     private static final String IMAGE_SOURCE = "https://raw.githubusercontent.com/PaulinaSadowska/GlideHeadersDemo/master/images/neko.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        catImage = (ImageView) findViewById(R.id.loaded_image);
+        catImageView = (ImageView) findViewById(R.id.loaded_image);
         downloadImage();
     }
 
     private void downloadImage() {
         Glide.with(this)
                 .load(Headers.getUrlWithHeaders(IMAGE_SOURCE))
-                .into(catImage);
+                .into(catImageView);
     }
 
 
