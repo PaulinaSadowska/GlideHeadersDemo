8002baf7ab261c5f9dadf8ec232de068d8f75a12
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-09-03 14:17:12 +0200

    fetching image with Glide

--- app/src/main/java/com/nekodev/paulina/sadowska/glideheadersdemo/MainActivity.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/glideheadersdemo/MainActivity.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView catImage;
+    private static final String IMAGE_SOURCE = "https://raw.githubusercontent.com/PaulinaSadowska/GlideHeadersDemo/master/images/neko.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         catImage = (ImageView) findViewById(R.id.loaded_image);
+        downloadImage();
     }
 
-
+    private void downloadImage() {
+        Glide.with(this)
+                .load(IMAGE_SOURCE)
+                .into(catImage);
+    }
 }
