b2ce060a1067cf4e05495735e5adab2f9e84f032
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-09-04 14:38:40 +0200

    HEaderLoader and HeaderLoaderFactory moved to separate files

---
+++ app/src/main/java/com/nekodev/paulina/sadowska/glideheadersdemo/glide/HeaderLoader.java
@@ -0,0 +1,32 @@
+package com.nekodev.paulina.sadowska.glideheadersdemo.glide;
+
+import android.content.Context;
+
+import com.bumptech.glide.load.model.Headers;
+import com.bumptech.glide.load.model.LazyHeaders;
+import com.bumptech.glide.load.model.stream.BaseGlideUrlLoader;
+
+/**
+ * Created by Paulina Sadowska on 04.09.2016.
+ */
+class HeaderLoader extends BaseGlideUrlLoader<String> {
+
+    private static final String AUTHORIZATION = "QWERTYUIOPASDFGHJKL";
+    private static final Headers REQUEST_HEADERS = new LazyHeaders.Builder()
+            .addHeader("Authorization", AUTHORIZATION)
+            .build();
+
+    HeaderLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected String getUrl(String model, int width, int height) {
+        return model;
+    }
+
+    @Override
+    protected Headers getHeaders(String model, int width, int height) {
+        return REQUEST_HEADERS;
+    }
+}
